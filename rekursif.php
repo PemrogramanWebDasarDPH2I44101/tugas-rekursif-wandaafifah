d9b4e76b668266d38c978d95d1a24605aa15ed2b
wandaafifah <42725331+wandaafifah@users.noreply.github.com>
2018-09-10 19:25:01 +0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -1,10 +1,37 @@
 <?php
-
-function rekursif($i){
-  echo "$i <br>";
-  $i--;
-  if($i>0)
-    rekursif($i);
+function rekursif($col, $d){
+	global $i;
+	rekursif2($i, $col, $d);
+	echo "<br>";
+	if (--$col > $i) {
+		rekursif($col, $d);
+	}
 }
-rekursif(5);
+function rekursif2($j, $col, $print){
+	global $i;
+	if ($j > $i - $col) {
+		echo "$print";
+		rekursif2($j - 1, $col, $print);
+}
+}
+$i = 0;
+rekursif(5, "*");
+echo"<br>/*=============================*/<br><br>";
+$angka=5;
+function rekursif3($g,$h,$angka){
+	if ($g<=$angka) {
+		if ($h<=$g) {
+			echo $h;
+			$h++;
+			rekursif3($g,$h,$angka);
+		}
+		else{
+			echo"<br>";
+			$g++;
+			rekursif3($g,1,$angka);
+	}
+	}
+}
+	rekursif3(1,1,$angka);
+
 ?>
